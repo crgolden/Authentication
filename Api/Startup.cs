1925a3dedc81af29b7cb529a3c928a46f93a6e4a
Chris Golden <crgolden@msn.com>
2020-11-19 08:06:44 -0700

    Upgrading to .NET Core 5.0

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -29,16 +29,16 @@ namespace Authentication
 
         public Startup(IConfiguration configuration, IWebHostEnvironment environment)
         {
+            _entityFrameworkConnectionString = configuration?.GetConnectionString("Authentication");
+            _authority = configuration?.GetValue<string>("Authority");
+            _audience = configuration?.GetValue<string>("Audience");
+            _identityServerOptionsSection = configuration?.GetIdentityServerOptionsSection();
+            _facebookAppId = configuration?.GetValue<string>("FacebookAppId");
+            _facebookAppSecret = configuration?.GetValue<string>("FacebookAppSecret");
+            _serviceBusOptionsSection = configuration?.GetServiceBusOptionsOptionsSection();
+            _emailOptionsSection = configuration?.GetSendGridEmailOptionsSection();
+            _corsOrigins = configuration?.GetSection("CorsOrigins").Get<string[]>();
             _environment = environment;
-            _entityFrameworkConnectionString = configuration.GetConnectionString("Authentication");
-            _authority = configuration.GetValue<string>("Authority");
-            _audience = configuration.GetValue<string>("Audience");
-            _identityServerOptionsSection = configuration.GetIdentityServerOptionsSection();
-            _facebookAppId = configuration.GetValue<string>("FacebookAppId");
-            _facebookAppSecret = configuration.GetValue<string>("FacebookAppSecret");
-            _serviceBusOptionsSection = configuration.GetServiceBusOptionsOptionsSection();
-            _emailOptionsSection = configuration.GetSendGridEmailOptionsSection();
-            _corsOrigins = configuration.GetSection("CorsOrigins").Get<string[]>();
         }
 
         // This method gets called by the runtime. Use this method to add services to the container.
@@ -46,50 +46,82 @@ namespace Authentication
         {
             DefaultInboundClaimTypeMap.Clear();
             DefaultOutboundClaimTypeMap.Clear();
-            services.AddApplicationInsightsTelemetry(options =>
-            {
-            });
-            services.AddRazorPages(options =>
-            {
-            }).AddMvcOptions(options =>
-            {
-                options.Filters.Add(new ModelStatePageFilter());
-            }).SetCompatibilityVersion(Latest);
-            services.AddCors(options =>
-            {
-            });
-            services.Configure<CookieTempDataProviderOptions>(options => options.Cookie.IsEssential = true);
-            services.AddEntityFrameworkDataService<IdentityServerDbContext>(
-                options =>
-                {
-                },
-                options =>
-                {
-                    options.UseSqlServer(_entityFrameworkConnectionString, sqlServerOptions =>
+            services
+                .AddApplicationInsightsTelemetry(
+                    options: options =>
                     {
-                        sqlServerOptions.MigrationsAssembly("Authentication.Api");
-                    });
-                });
-            services.AddIdentityServer(
-                _identityServerOptionsSection,
-                _environment.IsDevelopment(),
-                options => options.SignIn.RequireConfirmedEmail = true,
-                operationalStoreOptionsAction: options => options.EnableTokenCleanup = true,
-                identityServerAuthenticationOptionsAction: options =>
-                {
-                    options.Authority = _authority;
-                    options.ApiName = _audience;
-                },
-                facebookOptionsAction: options =>
-                {
-                    options.AppId = _facebookAppId;
-                    options.AppSecret = _facebookAppSecret;
-                    options.Events.OnRemoteFailure = context => context.HandleRemoteFailure();
-                });
-            services.AddHealthChecks().AddDbContextCheck<IdentityServerDbContext>();
-            services.AddQueueClient(_serviceBusOptionsSection);
-            services.AddSendGridEmailService(_emailOptionsSection);
-            services.AddKendo();
+                    })
+                .AddRazorPages(
+                    configure: options =>
+                    {
+                    })
+                .AddMvcOptions(
+                    setupAction: options =>
+                    {
+                        options.Filters.Add(new ModelStatePageFilter());
+                    })
+                .SetCompatibilityVersion(Latest)
+                .Services
+                .AddCors(
+                    setupAction: options =>
+                    {
+                    })
+                .Configure<CookieTempDataProviderOptions>(
+                    configureOptions: options =>
+                    {
+                        options.Cookie.IsEssential = true;
+                    })
+                .AddEntityFrameworkDataService<IdentityServerDbContext>(
+                    configureOptions: options =>
+                    {
+                    },
+                    optionsAction: options =>
+                    {
+                        options.UseSqlServer(
+                            connectionString: _entityFrameworkConnectionString,
+                            sqlServerOptionsAction: sqlServerOptions =>
+                            {
+                                sqlServerOptions.MigrationsAssembly("Authentication.Api");
+                            });
+                    })
+                .AddIdentityServer(
+                    config: _identityServerOptionsSection,
+                    isDevelopment: _environment.IsDevelopment(),
+                    identityOptionsAction: options =>
+                    {
+                        options.SignIn.RequireConfirmedAccount = true;
+                    },
+                    identityServerOptionsAction: options =>
+                    {
+                    },
+                    configurationStoreOptionsAction: options =>
+                    {
+                    },
+                    operationalStoreOptionsAction: options =>
+                    {
+                        options.EnableTokenCleanup = true;
+                    },
+                    authenticationOptionsAction: options =>
+                    {
+                    },
+                    identityServerAuthenticationOptionsAction: options =>
+                    {
+                        options.Authority = _authority;
+                        options.ApiName = _audience;
+                    },
+                    facebookOptionsAction: options =>
+                    {
+                        options.AppId = _facebookAppId;
+                        options.AppSecret = _facebookAppSecret;
+                        options.Events.OnRemoteFailure = context => context.HandleRemoteFailure();
+                    })
+                .AddDatabaseDeveloperPageExceptionFilter()
+                .AddHealthChecks()
+                .AddDbContextCheck<IdentityServerDbContext>()
+                .Services
+                .AddQueueClient(_serviceBusOptionsSection)
+                .AddSendGridEmailService(_emailOptionsSection)
+                .AddKendo();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -100,20 +132,30 @@ namespace Authentication
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseStaticFiles();
-            app.UseCookiePolicy();
-            app.UseExceptionHandler(app1 => app1.Run(async context => await context.HandleException().ConfigureAwait(false)));
-            app.UseHttpsRedirection();
-            app.UseRouting();
-            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins(_corsOrigins));
-            app.UseHealthChecks("/health");
-            app.UseAuthentication();
-            app.UseAuthorization();
-            app.UseIdentityServer();
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapRazorPages();
-            });
+            app
+                .UseStaticFiles()
+                .UseCookiePolicy()
+                .UseExceptionHandler(
+                    configure: app1 =>
+                    {
+                        app1.Run(async context => await context.HandleException().ConfigureAwait(false));
+                    })
+                .UseHttpsRedirection()
+                .UseRouting()
+                .UseCors(
+                    configurePolicy: policy =>
+                    {
+                        policy.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins(_corsOrigins);
+                    })
+                .UseHealthChecks("/health")
+                .UseAuthentication()
+                .UseAuthorization()
+                .UseIdentityServer()
+                .UseEndpoints(
+                    configure: endpoints =>
+                    {
+                        endpoints.MapRazorPages();
+                    });
         }
     }
 }
